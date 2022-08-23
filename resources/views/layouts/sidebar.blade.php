ff9739c3ef991ff456cda949e8fb3ffd4375c3d1
Bachtiar <66485489+bachtiaramin13@users.noreply.github.com>
2022-08-23 12:21:30 +0700

    sidebar dan app

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,4 +1,3 @@
-
 <nav>
     <div class="main-navbar">
         <div id="mainnav">
@@ -12,103 +11,74 @@
                         <h6>Penilaian Kinerja </h6>
                     </div>
                 </li>
+                @if (auth()->user()->role == 'admin')
+                    <li class="dropdown">
+                        <a class="nav-link menu-title link-nav active" href="{{ route('admin.admin_v') }}"><i
+                                data-feather="users"></i><span>Dashboard</span>
+                            <div class="according-menu"><i class="fa fa-angle-right"></i></div>
+                        </a>
 
-                    @if (auth()->user()->role == 'admin')
-                        <li class="dropdown">
-                            <a class="nav-link menu-title link-nav" href="{{ route('admin.admin_v') }}"><i
-                                    data-feather="users"></i><span>Dashboard</span>
-                                <div class="according-menu"><i class="fa fa-angle-right"></i></div>
-                            </a>
-
-                        </li>
-                        <li class="dropdown">
-                            <a class="nav-link menu-title"><i data-feather="book-open"></i><span>Master Data
-                                    Presensi</span>
-                                <div class="according-menu"><i class="fa fa-angle-right"></i></div>
-                            </a>
-                            <ul class="nav-submenu menu-content" style="display: none;">
-                                <li><a href="{{ route('admin.datapresensi') }}" class="">Data Presensi</a>
-                                </li>
-                                <li><a href="{{ route('admin.rekapitulasi') }}" class="">Rekapitulasi</a>
-                                </li>
-                            </ul>
-                        </li>
-                        <li class="dropdown">
-                            <a class="nav-link menu-title"><i data-feather="edit-3"></i><span>Pengajuan</span>
-                                <div class="according-menu"><i class="fa fa-angle-right"></i></div>
-                            </a>
-                            <ul class="nav-submenu menu-content" style="display: none;">
-                                <li><a href="{{ route('admin.dataizin') }}" class="">Izin</a>
-                                </li>
-                                <li><a href="{{ route('admin.datacuti') }}" class="">Cuti</a>
-                                </li>
-                            </ul>
-                        </li>
-                    @endif
-                    @if (auth()->user()->role == 'karyawan')
-                        <li class="dropdown">
-                            <a id="karyawan_index"
-                                class="nav-link menu-title link-nav {{ routeActive('karyawan.index') }}"
-                                href="{{ route('karyawan.index') }}"><i data-feather="users"></i><span>Dashboard</span>
-                                <div class="according-menu"><i class="fa fa-angle-right"></i></div>
-                            </a>
-                        </li>
-                        <li class="sidebar-main-title">
-                            <div>
-                                <h6>Data dan Pengajuan</h6>
-                            </div>
-                        </li>
-                        {{-- @if (request()->routeIs('karyawan.datapresensi') || request()->routeIs('karyawan.datarekapitulasi'))
-                            <h6>Blok</h6>
-                        @else
-                            <h6>none</h6>
-                        @endif --}}
-                        <li class="dropdown">
-                            <a class="nav-link menu-title @if (request()->routeIs('karyawan.datapresensi') || request()->routeIs('karyawan.datarekapitulasi')) active @endif"><i
-                                    data-feather="book-open"></i><span>Data
-                                    Kehadiran</span>
-                                <div class="according-menu"><i class="fa fa-angle-right"></i></div>
-                            </a>
-                            <ul class="nav-submenu menu-content"
-                                style="display: @if (request()->routeIs('karyawan.datapresensi') || request()->routeIs('karyawan.datarekapitulasi')) block
-                            @else
-                                none @endif;">
-                                <li><a id="a_active" href="{{ route('karyawan.datapresensi') }}"
-                                        class="{{ routeActive('karyawan.datapresensi') }}">Data Presensi</a>
-                                </li>
-                                <li><a href="{{ route('karyawan.datarekapitulasi') }}"
-                                        class="{{ routeActive('karyawan.datarekapitulasi') }}">Data
-                                        Rekapitulasi</a>
-                                </li>
-                            </ul>
-                        </li>
-                        <li class="dropdown">
-                            <a class="nav-link menu-title @if (request()->routeIs('karyawan.izin') ||
-                                request()->routeIs('karyawan.store_izin') ||
-                                request()->routeIs('karyawan.cuti') ||
-                                request()->routeIs('karyawan.store_cuti')) active @endif"><i
-                                    data-feather="edit-3"></i><span>Pengajuan</span>
-                                <div class="according-menu"><i class="fa fa-angle-right"></i></div>
-                            </a>
-                            <ul class="nav-submenu menu-content"
-                                style="display: @if (request()->routeIs('karyawan.izin') ||
-                                    request()->routeIs('karyawan.store_izin') ||
-                                    request()->routeIs('karyawan.cuti') ||
-                                    request()->routeIs('karyawan.store_cuti')) block
-                                @else
-                                    none @endif;">
-                                <li><a href="{{ route('karyawan.izin') }}"
-                                        class="{{ routeActive('karyawan.izin') }}">Izin</a>
-                                </li>
-                                <li><a href="{{ route('karyawan.cuti') }}"
-                                        class="{{ routeActive('karyawan.cuti') }}">Cuti</a>
-                                </li>
-                            </ul>
-                        </li>
-                    @endif
-                </ul>
-            </div>
-            <div class="right-arrow" id="right-arrow"><i data-feather="arrow-right"></i></div>
+                    </li>
+                    <li class="dropdown">
+                        <a class="nav-link menu-title"><i data-feather="book-open"></i><span>Master Data Presensi</span>
+                            <div class="according-menu"><i class="fa fa-angle-right"></i></div>
+                        </a>
+                        <ul class="nav-submenu menu-content" style="display: none;">
+                            <li><a href="{{ route('admin.datapresensi') }}" class="">Data Presensi</a>
+                            </li>
+                            <li><a href="{{ route('admin.rekapitulasi') }}" class="">Rekapitulasi</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="dropdown">
+                        <a class="nav-link menu-title"><i data-feather="edit-3"></i><span>Pengajuan</span>
+                            <div class="according-menu"><i class="fa fa-angle-right"></i></div>
+                        </a>
+                        <ul class="nav-submenu menu-content" style="display: none;">
+                            <li><a href="{{ route('admin.dataizin') }}" class="">Izin</a>
+                            </li>
+                            <li><a href="{{ route('admin.datacuti') }}" class="">Cuti</a>
+                            </li>
+                        </ul>
+                    </li>
+                @endif
+                @if (auth()->user()->role == 'karyawan')
+                    <li class="dropdown">
+                        <a class="nav-link menu-title link-nav active" href="{{ route('karyawan.index') }}"><i
+                                data-feather="users"></i><span>Dashboard</span>
+                            <div class="according-menu"><i class="fa fa-angle-right"></i></div>
+                        </a>
+                    </li>
+                    <li class="sidebar-main-title">
+                        <div>
+                            <h6>Data dan Pengajuan</h6>
+                        </div>
+                    </li>
+                    <li class="dropdown">
+                        <a class="nav-link menu-title"><i data-feather="book-open"></i><span>Data
+                                Kehadiran</span>
+                            <div class="according-menu"><i class="fa fa-angle-right"></i></div>
+                        </a>
+                        <ul class="nav-submenu menu-content" style="display: none;">
+                            <li><a href="{{ route('karyawan.datapresensi') }}" class="">Data Presensi</a>
+                            </li>
+                            <li><a href="{{ route('karyawan.datarekapitulasi') }}" class="">Data Rekapitulasi</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="dropdown">
+                        <a class="nav-link menu-title"><i data-feather="edit-3"></i><span>Pengajuan</span>
+                            <div class="according-menu"><i class="fa fa-angle-right"></i></div>
+                        </a>
+                        <ul class="nav-submenu menu-content" style="display: none;">
+                            <li><a href="{{ route('karyawan.izin') }}" class="">Izin</a>
+                            </li>
+                            <li><a href="{{ route('karyawan.cuti') }}" class="">Cuti</a>
+                            </li>
+                        </ul>
+                    </li>
+                @endif
+            </ul>
         </div>
-    </nav>
-</header>
+    </div>
+</nav>
