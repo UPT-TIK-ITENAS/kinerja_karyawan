4c0bffa43b4427c11d1f0efa28a243e5e6dc86a4
Hasna Septi Dewi <91710674+hasnaasd05@users.noreply.github.com>
2022-11-24 11:10:23 +0700

    Revert "admin"
    This reverts commit a51a21635b1eed1a3dc9675a451e70490e7d7634.

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -64,8 +64,7 @@ class AdminController extends Controller
             return DataTables::of($data)
                 ->addIndexColumn()
                 ->editColumn('hari', function ($row) {
-                    $days =  ['MINGGU', 'SENIN', 'SELASA', 'RABU', 'KAMIS', 'JUMAT', 'SABTU'];
-                    return $days[$row->hari];
+                    return config('app.days')[$row->hari];
                 })
                 ->addColumn('duration', function ($row) {
                     if ($row->jam_masuk == NULL && $row->jam_siang == NULL && $row->jam_pulang != NULL) {
@@ -79,14 +78,14 @@ class AdminController extends Controller
                         $awal = Carbon::createFromFormat("Y-m-d H:i:s", $row->jam_siang);
                         $durationwork = $akhir->diff($awal)->format('%H:%I:%S');
                     } else if ($row->jam_masuk != NULL && $row->jam_siang == NULL && $row->jam_pulang != NULL) {
-                        if ($row->hari == 5) {
-                            $akhir = Carbon::parse('17:00:00')->format('H:i:s');
-                            $awal = Carbon::parse('13:30:00')->format('H:i:s');
+                        if ($row->hari == '5') {
+                            $akhir = Carbon::parse('13:00:00')->format('H:i:s');
+                            $awal = Carbon::parse($row->jam_masuk)->format('H:i:s');
                             $durasi = strtotime($akhir) - strtotime($awal);
                             $durationwork = Carbon::parse($durasi)->format('H:i:s');
                         } else {
-                            $akhir = Carbon::parse('17:00:00')->format('H:i:s');
-                            $awal = Carbon::parse('13:00:00')->format('H:i:s');
+                            $akhir = Carbon::parse('13:30:00')->format('H:i:s');
+                            $awal = Carbon::parse($row->jam_masuk)->format('H:i:s');
                             $durasi = strtotime($akhir) - strtotime($awal);
                             $durationwork = Carbon::parse($durasi)->format('H:i:s');
                         }
